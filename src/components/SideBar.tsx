337c2d189688e888e137ea78c0967165a8adec3e
Edivaldo Silva <piqueno43@gmail.com>
2021-10-29 11:06:49 -0300

    Chalange finish

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,28 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  onClickButton: (id: number) => void;
+}
+export function SideBar({genres, selectedGenreId, onClickButton}: SideBarProps) {
+  
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => onClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
